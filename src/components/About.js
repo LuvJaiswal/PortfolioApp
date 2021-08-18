b5189189b759e746aaee7a45777431d7d42c39b9
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-08-18 12:20:08 +0545

    icons added for offer

--- src/components/About.js
+++ src/components/About.js
@@ -1,46 +1,53 @@
 import React from 'react';
-import api from '../assets/images/icons/front.png'
+import front from '../assets/images/icons/front.png'
+import back from '../assets/images/icons/back.png'
+import api from '../assets/images/icons/restApi.png'
+import coder from '../assets/images/icons/coder.jpg'
+import event from '../assets/images/icons/eventmanagement.jpg'
+import mobile from '../assets/images/icons/mobile.jpg'
+import web from '../assets/images/icons/webapp.png'
+import download from '../assets/images/icons/download.png'
 import Skillcard from './Skillcard';
 
 const skills = [
     {
-        icon: api,
-        title: "Frontend",
+        icon: front,
+        title: "Frontend Development",
         about: "work of Frontend",
     },
     {
-        icon: api,
-        title: "Backend",
+        icon: back,
+        title: "Backend Development",
         about: "work of Backend",
     },
     {
         icon: api,
-        title: "Backend",
+        title: "API DEVELOPMENT",
+        about: "work of API",
+    },
+    {
+        icon: coder,
+        title: "Competitive coder",
         about: "work of Backend",
     },
     {
-        icon: api,
-        title: "Backend",
+        icon: event,
+        title: "Event Management",
         about: "work of Backend",
     },
     {
-        icon: api,
-        title: "Backend",
+        icon: mobile,
+        title: "Mobile App Development",
         about: "work of Backend",
     },
     {
-        icon: api,
-        title: "Backend",
+        icon: web,
+        title: "Web App Development",
         about: "work of Backend",
     },
     {
-        icon: api,
-        title: "Backend",
-        about: "work of Backend",
-    },
-    {
-        icon: api,
-        title: "Backend",
+        icon: download,
+        title: "UI/UX Designer",
         about: "work of Backend",
     }
 
