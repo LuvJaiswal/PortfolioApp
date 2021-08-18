de7692d7702004e67a946e53fff75022f8d4dc50
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-08-18 10:59:17 +0545

    about offer design beginS

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,43 @@
+import React from 'react';
+import api from '../assets/images/icons/front.png'
+
+const skills = [
+    {
+        icon: api,
+        title: "Frontend",
+        about: "work of Frontend",
+    },
+    {
+        icon: api,
+        title: "Backend",
+        about: "work of Backend",
+    }
+]
+
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div className="about">
+            <h6 className="about_intro">This is about page</h6>
+            <div className="container about_container">
+                <h6 className="about_heading"> What I offer </h6>
+                <div className="row">
+                    {
+                        skills.map(skill =>
+                            <div className="col-lg-6">
+                                <div className="skill-card">
+                                    <img src={skill.icon} alt="icon" className="skill-card_icon" />
+                                    <div className="skill-card-body">
+                                        <h6 className="skill-card_title">{skill.title}</h6>
+                                        <p className="skill_card_content">{skill.about}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+         
         </div>
-    );
-};
-
-export default About;
+           );
+}
+            export default About;
