09bc6796f397e86bef66b064664402498f7745b9
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-08-19 15:11:03 +0545

    content 30% kept for resume design

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,68 @@
 const Resume = () => {
     return (
-        <div>
-            <h1>This is Resume page</h1>
+        <div className="container resume">
+            <div className="row">
+                <div className="col-lg-6 resume-card">
+                    <h4 className="resume-card_heading">
+                        Education
+                    </h4>
+                    <div className="resume-card_body">
+                        <h5 className="resume-card_title">
+                            Bachelor's in Computer Science
+                        </h5>
+                        <p className="resume-card_name">
+                            Academic of Technology
+                        </p>
+                        <p className="resume-card_details">
+                            This is s..,.....
+                        </p>
+
+                    </div>
+                </div>
+                <div className="col-lg-6 resume-card">
+                    <h4 className="resume-card_heading">
+                        Experience
+                    </h4>
+                    <div className="resume-card_body">
+                        <h5 className="resume-card_title">
+                            Full Stack Mobile and Web Developer
+                        </h5>
+                        <p className="resume-card_name">
+                            have worked on .......
+                        </p>
+                        <p className="resume-card_details">
+                            This is s..,.....
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-6 resume-languages">
+                    <h5 className="resume-language_heading">
+                        Language and Framework
+                    </h5>
+                    <div className="resume-language_body">
+                        {
+
+                        }
+                    </div>
+                </div>
+                <div className="col-lg-6 resume-languages">
+                    <h5 className="resume-language_heading">
+                        Tools and Software
+                    </h5>
+                    <div className="resume-language_body">
+                        {
+
+                        }
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
+
     );
 };
 
