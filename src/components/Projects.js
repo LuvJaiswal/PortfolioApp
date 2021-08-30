cd6fba07b0ff0b752a3a248ab9247d0c015b61a0
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2021-08-30 11:47:02 +0545

    adding sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 import data_projects from './data/projects_data'
 import ProjectCard from './ProjectCard';
 const Projects = () => {
+    const [projects, setProjects] = useState(data_projects)
     return (
 
         <div className="container projects">
+            <div className="projects_navbar">
+                <div>Java</div>
+                <div>Dart</div>
+                <div>React</div>
+                <div>Bootstrap</div>
+                <div>PHP/Laravel</div>
+                <div>dotNet</div>
+
+
+            </div>
 
             <div className="row">
                 {
